7e08a6c733e943a23fa6084ad11fb6697c893110
savosin_sergey@mail.ru <savosin_sergey@mail.ru>
2019-01-13 08:32:27 +0300

    Настройка формы FormMain

--- dbDataSearch/FormMain.Designer.cs
+++ dbDataSearch/FormMain.Designer.cs
@@ -28,20 +28,335 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.toolStripContainerMain = new System.Windows.Forms.ToolStripContainer();
+            this.statusStripMain = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.kryptonSplitContainer1 = new ComponentFactory.Krypton.Toolkit.KryptonSplitContainer();
+            this.menuStripMain = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMain = new System.Windows.Forms.ToolStrip();
+            this.btnLightScheme = new System.Windows.Forms.ToolStripButton();
+            this.btnDarkScheme = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripComboBox2 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.lblFindEntity = new System.Windows.Forms.ToolStripLabel();
+            this.btnFindEntity = new System.Windows.Forms.ToolStripButton();
+            this.kryptonHeaderGroup1 = new ComponentFactory.Krypton.Toolkit.KryptonHeaderGroup();
+            this.kryptonHeaderGroup2 = new ComponentFactory.Krypton.Toolkit.KryptonHeaderGroup();
+            this.treeEntities = new ComponentFactory.Krypton.Toolkit.KryptonTreeView();
+            this.gridEntityValues = new ComponentFactory.Krypton.Toolkit.KryptonDataGridView();
+            this.kryptonManagerMain = new ComponentFactory.Krypton.Toolkit.KryptonManager(this.components);
+            this.kryptonPalette_Office2010_Black = new ComponentFactory.Krypton.Toolkit.KryptonPalette(this.components);
+            this.kryptonPalette_Office2010_Blue = new ComponentFactory.Krypton.Toolkit.KryptonPalette(this.components);
+            this.toolStripContainerMain.BottomToolStripPanel.SuspendLayout();
+            this.toolStripContainerMain.ContentPanel.SuspendLayout();
+            this.toolStripContainerMain.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainerMain.SuspendLayout();
+            this.statusStripMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonSplitContainer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonSplitContainer1.Panel1)).BeginInit();
+            this.kryptonSplitContainer1.Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonSplitContainer1.Panel2)).BeginInit();
+            this.kryptonSplitContainer1.Panel2.SuspendLayout();
+            this.kryptonSplitContainer1.SuspendLayout();
+            this.menuStripMain.SuspendLayout();
+            this.toolStripMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonHeaderGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonHeaderGroup1.Panel)).BeginInit();
+            this.kryptonHeaderGroup1.Panel.SuspendLayout();
+            this.kryptonHeaderGroup1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonHeaderGroup2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonHeaderGroup2.Panel)).BeginInit();
+            this.kryptonHeaderGroup2.Panel.SuspendLayout();
+            this.kryptonHeaderGroup2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridEntityValues)).BeginInit();
             this.SuspendLayout();
+            // 
+            // toolStripContainerMain
+            // 
+            // 
+            // toolStripContainerMain.BottomToolStripPanel
+            // 
+            this.toolStripContainerMain.BottomToolStripPanel.Controls.Add(this.statusStripMain);
+            // 
+            // toolStripContainerMain.ContentPanel
+            // 
+            this.toolStripContainerMain.ContentPanel.Controls.Add(this.kryptonSplitContainer1);
+            this.toolStripContainerMain.ContentPanel.Size = new System.Drawing.Size(800, 379);
+            this.toolStripContainerMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainerMain.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainerMain.Name = "toolStripContainerMain";
+            this.toolStripContainerMain.Size = new System.Drawing.Size(800, 450);
+            this.toolStripContainerMain.TabIndex = 0;
+            this.toolStripContainerMain.Text = "toolStripContainer1";
+            // 
+            // toolStripContainerMain.TopToolStripPanel
+            // 
+            this.toolStripContainerMain.TopToolStripPanel.Controls.Add(this.menuStripMain);
+            this.toolStripContainerMain.TopToolStripPanel.Controls.Add(this.toolStripMain);
+            // 
+            // statusStripMain
+            // 
+            this.statusStripMain.Dock = System.Windows.Forms.DockStyle.None;
+            this.statusStripMain.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.statusStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStripMain.Location = new System.Drawing.Point(0, 0);
+            this.statusStripMain.Name = "statusStripMain";
+            this.statusStripMain.RenderMode = System.Windows.Forms.ToolStripRenderMode.ManagerRenderMode;
+            this.statusStripMain.Size = new System.Drawing.Size(800, 22);
+            this.statusStripMain.TabIndex = 0;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // kryptonSplitContainer1
+            // 
+            this.kryptonSplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonSplitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.kryptonSplitContainer1.Name = "kryptonSplitContainer1";
+            // 
+            // kryptonSplitContainer1.Panel1
+            // 
+            this.kryptonSplitContainer1.Panel1.Controls.Add(this.kryptonHeaderGroup1);
+            // 
+            // kryptonSplitContainer1.Panel2
+            // 
+            this.kryptonSplitContainer1.Panel2.Controls.Add(this.kryptonHeaderGroup2);
+            this.kryptonSplitContainer1.Size = new System.Drawing.Size(800, 379);
+            this.kryptonSplitContainer1.SplitterDistance = 266;
+            this.kryptonSplitContainer1.TabIndex = 0;
+            // 
+            // menuStripMain
+            // 
+            this.menuStripMain.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStripMain.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.menuStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStripMain.Location = new System.Drawing.Point(0, 0);
+            this.menuStripMain.Name = "menuStripMain";
+            this.menuStripMain.Size = new System.Drawing.Size(800, 24);
+            this.menuStripMain.TabIndex = 0;
+            this.menuStripMain.Text = "menuStripMain";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // toolStripMain
+            // 
+            this.toolStripMain.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStripMain.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnLightScheme,
+            this.btnDarkScheme,
+            this.toolStripLabel1,
+            this.toolStripComboBox1,
+            this.toolStripLabel2,
+            this.toolStripComboBox2,
+            this.lblFindEntity,
+            this.toolStripTextBox1,
+            this.btnFindEntity});
+            this.toolStripMain.Location = new System.Drawing.Point(3, 24);
+            this.toolStripMain.Name = "toolStripMain";
+            this.toolStripMain.Size = new System.Drawing.Size(745, 25);
+            this.toolStripMain.TabIndex = 1;
+            // 
+            // btnLightScheme
+            // 
+            this.btnLightScheme.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnLightScheme.Image = ((System.Drawing.Image)(resources.GetObject("btnLightScheme.Image")));
+            this.btnLightScheme.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnLightScheme.Name = "btnLightScheme";
+            this.btnLightScheme.Size = new System.Drawing.Size(23, 22);
+            this.btnLightScheme.Text = "Switch to light scheme";
+            this.btnLightScheme.Click += new System.EventHandler(this.btnLightScheme_Click);
+            // 
+            // btnDarkScheme
+            // 
+            this.btnDarkScheme.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnDarkScheme.Image = ((System.Drawing.Image)(resources.GetObject("btnDarkScheme.Image")));
+            this.btnDarkScheme.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnDarkScheme.Name = "btnDarkScheme";
+            this.btnDarkScheme.Size = new System.Drawing.Size(23, 22);
+            this.btnDarkScheme.Text = "Switch to dark scheme";
+            this.btnDarkScheme.Click += new System.EventHandler(this.btnDarkScheme_Click);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(86, 22);
+            this.toolStripLabel1.Text = "toolStripLabel1";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 25);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(86, 22);
+            this.toolStripLabel2.Text = "toolStripLabel2";
+            // 
+            // toolStripComboBox2
+            // 
+            this.toolStripComboBox2.Name = "toolStripComboBox2";
+            this.toolStripComboBox2.Size = new System.Drawing.Size(121, 25);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 25);
+            // 
+            // lblFindEntity
+            // 
+            this.lblFindEntity.Name = "lblFindEntity";
+            this.lblFindEntity.Size = new System.Drawing.Size(113, 22);
+            this.lblFindEntity.Text = "Строка для поиска:";
+            // 
+            // btnFindEntity
+            // 
+            this.btnFindEntity.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnFindEntity.Image = ((System.Drawing.Image)(resources.GetObject("btnFindEntity.Image")));
+            this.btnFindEntity.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnFindEntity.Name = "btnFindEntity";
+            this.btnFindEntity.Size = new System.Drawing.Size(23, 22);
+            this.btnFindEntity.Text = "toolStripButton3";
+            // 
+            // kryptonHeaderGroup1
+            // 
+            this.kryptonHeaderGroup1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonHeaderGroup1.HeaderStylePrimary = ComponentFactory.Krypton.Toolkit.HeaderStyle.Secondary;
+            this.kryptonHeaderGroup1.HeaderVisibleSecondary = false;
+            this.kryptonHeaderGroup1.Location = new System.Drawing.Point(0, 0);
+            this.kryptonHeaderGroup1.Name = "kryptonHeaderGroup1";
+            // 
+            // kryptonHeaderGroup1.Panel
+            // 
+            this.kryptonHeaderGroup1.Panel.Controls.Add(this.treeEntities);
+            this.kryptonHeaderGroup1.Size = new System.Drawing.Size(266, 379);
+            this.kryptonHeaderGroup1.TabIndex = 0;
+            // 
+            // kryptonHeaderGroup2
+            // 
+            this.kryptonHeaderGroup2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonHeaderGroup2.HeaderStylePrimary = ComponentFactory.Krypton.Toolkit.HeaderStyle.Secondary;
+            this.kryptonHeaderGroup2.HeaderVisibleSecondary = false;
+            this.kryptonHeaderGroup2.Location = new System.Drawing.Point(0, 0);
+            this.kryptonHeaderGroup2.Name = "kryptonHeaderGroup2";
+            // 
+            // kryptonHeaderGroup2.Panel
+            // 
+            this.kryptonHeaderGroup2.Panel.Controls.Add(this.gridEntityValues);
+            this.kryptonHeaderGroup2.Size = new System.Drawing.Size(529, 379);
+            this.kryptonHeaderGroup2.TabIndex = 0;
+            // 
+            // treeEntities
+            // 
+            this.treeEntities.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeEntities.Location = new System.Drawing.Point(0, 0);
+            this.treeEntities.Name = "treeEntities";
+            this.treeEntities.Size = new System.Drawing.Size(264, 356);
+            this.treeEntities.TabIndex = 0;
+            // 
+            // gridEntityValues
+            // 
+            this.gridEntityValues.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridEntityValues.Location = new System.Drawing.Point(0, 0);
+            this.gridEntityValues.Name = "gridEntityValues";
+            this.gridEntityValues.Size = new System.Drawing.Size(527, 356);
+            this.gridEntityValues.TabIndex = 0;
+            // 
+            // kryptonPalette_Office2010_Black
+            // 
+            this.kryptonPalette_Office2010_Black.BasePaletteMode = ComponentFactory.Krypton.Toolkit.PaletteMode.Office2007Black;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStripContainerMain);
+            this.MainMenuStrip = this.menuStripMain;
             this.Name = "FormMain";
             this.Text = "-= dbDataSearch =-";
+            this.toolStripContainerMain.BottomToolStripPanel.ResumeLayout(false);
+            this.toolStripContainerMain.BottomToolStripPanel.PerformLayout();
+            this.toolStripContainerMain.ContentPanel.ResumeLayout(false);
+            this.toolStripContainerMain.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainerMain.TopToolStripPanel.PerformLayout();
+            this.toolStripContainerMain.ResumeLayout(false);
+            this.toolStripContainerMain.PerformLayout();
+            this.statusStripMain.ResumeLayout(false);
+            this.statusStripMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonSplitContainer1.Panel1)).EndInit();
+            this.kryptonSplitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonSplitContainer1.Panel2)).EndInit();
+            this.kryptonSplitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonSplitContainer1)).EndInit();
+            this.kryptonSplitContainer1.ResumeLayout(false);
+            this.menuStripMain.ResumeLayout(false);
+            this.menuStripMain.PerformLayout();
+            this.toolStripMain.ResumeLayout(false);
+            this.toolStripMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonHeaderGroup1.Panel)).EndInit();
+            this.kryptonHeaderGroup1.Panel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonHeaderGroup1)).EndInit();
+            this.kryptonHeaderGroup1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonHeaderGroup2.Panel)).EndInit();
+            this.kryptonHeaderGroup2.Panel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonHeaderGroup2)).EndInit();
+            this.kryptonHeaderGroup2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridEntityValues)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainerMain;
+        private System.Windows.Forms.MenuStrip menuStripMain;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private ComponentFactory.Krypton.Toolkit.KryptonSplitContainer kryptonSplitContainer1;
+        private System.Windows.Forms.StatusStrip statusStripMain;
+        private System.Windows.Forms.ToolStrip toolStripMain;
+        private System.Windows.Forms.ToolStripButton btnLightScheme;
+        private System.Windows.Forms.ToolStripButton btnDarkScheme;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox2;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripLabel lblFindEntity;
+        private System.Windows.Forms.ToolStripButton btnFindEntity;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private ComponentFactory.Krypton.Toolkit.KryptonHeaderGroup kryptonHeaderGroup1;
+        private ComponentFactory.Krypton.Toolkit.KryptonTreeView treeEntities;
+        private ComponentFactory.Krypton.Toolkit.KryptonHeaderGroup kryptonHeaderGroup2;
+        private ComponentFactory.Krypton.Toolkit.KryptonDataGridView gridEntityValues;
+        private ComponentFactory.Krypton.Toolkit.KryptonManager kryptonManagerMain;
+        private ComponentFactory.Krypton.Toolkit.KryptonPalette kryptonPalette_Office2010_Black;
+        private ComponentFactory.Krypton.Toolkit.KryptonPalette kryptonPalette_Office2010_Blue;
     }
 }
 
--- dbDataSearch/FormMain.cs
+++ dbDataSearch/FormMain.cs
@@ -16,5 +16,21 @@ namespace dbDataSearch
         {
             InitializeComponent();
         }
+
+        private void btnLightScheme_Click(object sender, EventArgs e)
+        {
+            kryptonManagerMain.GlobalPalette = kryptonPalette_Office2010_Blue;
+            treeEntities.StateCommon.Back.Color1 = Color.Empty;
+            treeEntities.StateCommon.Node.Content.ShortText.Color1 = Color.Black;
+            treeEntities.StateCommon.Node.Back.Color1 = Color.PaleGreen;
+        }
+
+        private void btnDarkScheme_Click(object sender, EventArgs e)
+        {
+            kryptonManagerMain.GlobalPalette = kryptonPalette_Office2010_Black;
+            treeEntities.StateCommon.Back.Color1 = Color.DimGray;
+            treeEntities.StateCommon.Node.Content.ShortText.Color1 = Color.White;
+            treeEntities.StateCommon.Node.Back.Color1 = Color.RoyalBlue;
+        }
     }
 }
